f3fa651d876b5d34f87b89ca1073f2427187ef29
Ihsan Md. Al-Aqib <ihsan.alaqib@bjitacademy.com>
2022-12-07 08:32:59 +0600

    home page connected

--- app/src/main/java/com/shokal/affirmation/HomeActivity.kt
+++ app/src/main/java/com/shokal/affirmation/HomeActivity.kt
@@ -11,9 +11,11 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
 
         val userList=findViewById<Button>(R.id.user_list)
+        val email= intent.getStringExtra("email")
 
         userList.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("email", email)
             startActivity(intent)
         }
 
